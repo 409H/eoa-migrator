af7a6c3d9e28107ff7875ec87fc245815d09f2ab
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2022-03-21 14:44:46 +0000

    Fix bug if average gas is not an absolute number

--- src/components/TotalCostEstimator/index.tsx
+++ src/components/TotalCostEstimator/index.tsx
@@ -55,8 +55,8 @@ const TotalCostEstimator = (props: IProps) => {
         // Get the average
         return gas;
     }
-
-    const totalGas = BigNumber.from(avgGasLimit * totalTransactions).mul(gasAverage)
+    
+    const totalGas = BigNumber.from((avgGasLimit * totalTransactions).toString()).mul(Math.ceil(gasAverage))
 
     return(
         <Container>
